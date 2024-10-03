a6411f531c5f8c56ac9a1ee27165b88d3c702ab0
AzkaKhizer <144557215+AzkaKhizer@users.noreply.github.com>
2024-10-03 20:59:52 +0500

    layout.tsx

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -4,7 +4,6 @@ import "./globals.css";
 import Header from "./../../components/Header"
 import Footer from "./../../components/Footer"
 import Hero from "./../../components/Hero"
-import Link from "next/link";
 
 const geistSans = localFont({
   src: "./../../../public/fonts/GeistVF.woff",
