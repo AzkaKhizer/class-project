2913723e1c8bd982b7b2b7caf9cafb90a6952838
AzkaKhizer <144557215+AzkaKhizer@users.noreply.github.com>
2024-10-03 21:28:25 +0500

    Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,10 +3,10 @@ import Image from 'next/image'
 
 function Hero () {
   return (
-    <div className='flex bg-gray-800 text-white h-screen mr-20 ml-20 '>
+    <div className="flex bg-gray-800 text-white h-screen mr-20 ml-20 ">
       <div className= "flex-1 flex flex-col items-start justify-center px-8">
-        <h1 className='text-4xl font-bold mb-4 text-center'>Hi, I'm Azka Khizer, a Web Developer Specializing in Modern Web Technologies</h1>
-        <p className='text-2xl text-white max-w-md text-center pl-11 ml-20'>I create fast, responsive, and user-friendly websites using HTML, CSS, TypeScript, and Next.js. With a strong foundation in front-end and back-end development, I build innovative solutions that not only look great but also provide seamless functionality. Let's bring your ideas to life on the web!</p>
+        <h1 className="text-4xl font-bold mb-4 text-center">Hi, I am Azka Khizer, a Web Developer Specializing in Modern Web Technologies</h1>
+        <p className="text-2xl text-white max-w-md text-center pl-11 ml-20">I create fast, responsive, and user-friendly websites using HTML, CSS, TypeScript, and Next.js. With a strong foundation in front-end and back-end development, I build innovative solutions that not only look great but also provide seamless functionality. Let's bring your ideas to life on the web!</p>
         </div>
         <div className="w-1/4 h-auto ml-auto flex items-center justify-end pr-20 mr-20">
     <Image 
